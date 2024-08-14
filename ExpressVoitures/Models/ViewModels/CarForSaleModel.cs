76a471dca08dfb8e1a061160d3c44a3fe7274da8
Romain BUREL <generic3@diademe.fr>
2024-08-14 11:41:16 +0200

    feat: update models for validation rules

--- ExpressVoitures/Models/ViewModels/CarForSaleModel.cs
+++ ExpressVoitures/Models/ViewModels/CarForSaleModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ExpressVoitures.Models.ViewModels
 {
-    public class CarForSaleModel
+    public class CarForSaleModel: IValidatableObject
     {
         public int IdCarForSale { get; set; }
 
@@ -13,6 +13,7 @@ namespace ExpressVoitures.Models.ViewModels
 
         [Required(ErrorMessage = "La sélection d'une date de disponibilité est obligatoire.")]
         [DisplayName("Date de disponibilité")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DateOfAvailabilityForSale { get; set; }
 
         [DisplayName("Prix de vente")]
@@ -24,11 +25,31 @@ namespace ExpressVoitures.Models.ViewModels
         public string? ImagePath { get; set; }
 
         [DisplayName("Date de vente")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime? DateOfSale { get; set; }
 
         public List<Car>? Cars { get; set; }
 
         [DisplayName("Voiture")]
         public string? CarLabel { get; set; }
-    }
+
+        public Car? Car { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			var validationResults = new List<ValidationResult>();
+
+			if (Car != null && DateOfAvailabilityForSale < Car.DateOfBuy)
+			{
+				validationResults.Add(new ValidationResult("La date de disponibilité à la vente ne peut pas être antérieure à la date d'achat.", new[] { nameof(DateOfAvailabilityForSale) }));
+			}
+
+			if (DateOfSale.HasValue && DateOfSale.Value < DateOfAvailabilityForSale)
+			{
+				validationResults.Add(new ValidationResult("La date de vente ne peut pas être antérieure à la date de disponibilité à la vente.", new[] { nameof(DateOfSale) }));
+			}
+
+			return validationResults;
+		}
+	}
 }
--- ExpressVoitures/Models/ViewModels/CarModel.cs
+++ ExpressVoitures/Models/ViewModels/CarModel.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel;
+﻿using ExpressVoitures.Models.Entities;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace ExpressVoitures.Models.ViewModels
 {
-    public class CarModel
+    public class CarModel: IValidatableObject
     {
         public int IdCar { get; set; }
 
@@ -12,7 +13,8 @@ namespace ExpressVoitures.Models.ViewModels
 
         [Required(ErrorMessage = "La saisie de l'année est obligatoire.")]
         [DisplayName("Année de mise en circulation du véhicule")]
-        public int Year { get; set; }
+		[RangeUntilCurrentYear(1990, ErrorMessage = "L'année doit être comprise entre 1990 et aujourd'hui.")]
+		public int Year { get; set; }
 
         [Required(ErrorMessage = "La saisie de la marque est obligatoire.")]
         [DisplayName("Marque du véhicule")]
@@ -27,6 +29,7 @@ namespace ExpressVoitures.Models.ViewModels
 
         [Required(ErrorMessage = "La saisie de la date d'achat est obligatoire.")]
         [DisplayName("Date d'achat du véhicule")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DateOfBuy { get; set; }
 
         [Required(ErrorMessage = "La saisie du prix d'achat est obligatoire.")]
@@ -37,6 +40,7 @@ namespace ExpressVoitures.Models.ViewModels
         public string? RepairDescription { get; set; }
 
         [DisplayName("Date de la réparation")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime? DateOfRepair { get; set; }
 
         [Required(ErrorMessage = "La saisie du coût de réparation est obligatoire (mettre 0 si aucun).")]
@@ -45,5 +49,22 @@ namespace ExpressVoitures.Models.ViewModels
 
         [DisplayName("Véhicule plus disponible")]
         public bool NoMoreAvailable { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var validationResults = new List<ValidationResult>();
+
+            if (DateOfBuy.Year < Year)
+            {
+                validationResults.Add(new ValidationResult("La date d'achat ne peut pas être inférieure à l'année de mise en circulation.", new[] { nameof(DateOfBuy) }));
+            }
+
+            if (DateOfRepair.HasValue && DateOfRepair.Value < DateOfBuy)
+            {
+                validationResults.Add(new ValidationResult("La date de réparation ne peut pas être antérieure à la date d'achat.", new[] { nameof(DateOfRepair) }));
+            }
+
+            return validationResults;
+        }
     }
 }
