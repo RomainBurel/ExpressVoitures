9d60de21649c1fdb462265a67636febfd79d9f03
Romain BUREL <generic3@diademe.fr>
2024-08-14 08:17:49 +0200

    feat: add view models

---
+++ ExpressVoitures/Models/ViewModels/CarForSaleModel.cs
@@ -0,0 +1,34 @@
+﻿using ExpressVoitures.Models.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpressVoitures.ViewModels
+{
+    public class CarForSaleModel
+    {
+        public int IdCarForSale { get; set; }
+
+        [DisplayName("Voiture")]
+        public int IdCar { get; set; }
+
+        [Required(ErrorMessage = "La sélection d'une date de disponibilité est obligatoire.")]
+        [DisplayName("Date de disponibilité")]
+        public DateTime DateOfAvailabilityForSale { get; set; }
+
+        [DisplayName("Prix de vente")]
+        public double SellingPrice { get; set; }
+
+        public string? Description { get; set; }
+
+        [DisplayName("Photo")]
+        public string? ImagePath { get; set; }
+
+        [DisplayName("Date de vente")]
+        public DateTime? DateOfSale { get; set; }
+
+        public List<Car>? Cars { get; set; }
+
+        [DisplayName("Voiture")]
+        public string? CarLabel { get; set; }
+    }
+}
---
+++ ExpressVoitures/Models/ViewModels/CarModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpressVoitures.ViewModels
+{
+    public class CarModel
+    {
+        public int IdCar { get; set; }
+
+        [DisplayName("Numéro d'identification du véhicule")]
+        public string? VIN { get; set; }
+
+        [Required(ErrorMessage = "La saisie de l'année est obligatoire.")]
+        [DisplayName("Année de mise en circulation du véhicule")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "La saisie de la marque est obligatoire.")]
+        [DisplayName("Marque du véhicule")]
+        public string Brand { get; set; }
+
+        [Required(ErrorMessage = "La saisie du modèle est obligatoire.")]
+        [DisplayName("Modèle du véhicule")]
+        public string Model { get; set; }
+
+        [DisplayName("Finition du véhicule")]
+        public string? Finish { get; set; }
+
+        [Required(ErrorMessage = "La saisie de la date d'achat est obligatoire.")]
+        [DisplayName("Date d'achat du véhicule")]
+        public DateTime DateOfBuy { get; set; }
+
+        [Required(ErrorMessage = "La saisie du prix d'achat est obligatoire.")]
+        [DisplayName("Prix d'achat du véhicule")]
+        public double Price { get; set; }
+
+        [DisplayName("Réparation(s) effectuée(s) sur le véhicule")]
+        public string? RepairDescription { get; set; }
+
+        [DisplayName("Date de la réparation")]
+        public DateTime? DateOfRepair { get; set; }
+
+        [Required(ErrorMessage = "La saisie du coût de réparation est obligatoire (mettre 0 si aucun).")]
+        [DisplayName("Coût de la réparation")]
+        public double RepairCost { get; set; }
+
+        [DisplayName("Véhicule plus disponible")]
+        public bool NoMoreAvailable { get; set; }
+    }
+}
